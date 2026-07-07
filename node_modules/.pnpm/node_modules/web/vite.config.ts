42571a5e7cebd0ed42ec003eba2b55b4b93e366f
Biswajeet Bishoyi <bishoyibiswajeet@gmail.com>
2026-07-07 19:20:26 +0530

    fix: adjust vite base url for vercel deployment

--- node_modules/.pnpm/node_modules/web/vite.config.ts
+++ node_modules/.pnpm/node_modules/web/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/BeamLab/',
+  base: process.env.VERCEL ? '/' : '/BeamLab/',
   plugins: [
     react(),
     tailwindcss(),
